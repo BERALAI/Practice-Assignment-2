28b785acd3c5720debd02c0fd5511d4cfabc3b4e
BERALAI <99305084+BERALAI@users.noreply.github.com>
2022-02-16 02:45:39 -0800

    Update index.php

--- index.php
+++ index.php
@@ -73,6 +73,21 @@
         </div>
       </div>
       <hr>
+          <div id="skills" class="content-item">
+        <div class="vertical-center">
+          <h2>Skills</h2>
+          <?php foreach($json_data['skills'] AS $skills) { ?>
+              <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                  <h3 class="mb-0"><?php echo $skills['languages']; ?></h3>
+                  <div class="subheading mb-3"><?php echo $experience['employer']; ?></div>
+                  <p><?php echo $skills['programs']; ?></p>
+                </div>
+              </div>
+          <?php  } ?>
+        </div>
+      </div>
+      <hr>
     </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
